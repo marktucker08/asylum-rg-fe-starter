a5d0894f811842c8f4eea78b0255fbf6e8074daa
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2023-06-12 18:32:07 -0600

    minor changes on profile page

--- src/components/pages/profile-page.js
+++ src/components/pages/profile-page.js
@@ -23,20 +23,22 @@ export const ProfilePage = () => {
           <div className="profile__header">
             <img src={user.picture} alt="Profile" className="profile__avatar" />
             <div className="profile__headline">
-              <h2 className="profile__title">{user.name}</h2>
+              <h2 className="profile__title">{user.nickname}</h2>
               <span className="profile__description">{user.email}</span>
             </div>
           </div>
-          {/* <div className="profile__details">
+          <div className="profile__details">
             <div className="code-snippet">
-                <span className="code-snippet__title">{user.title}</span>
-                    <div className="code-snippet__container">
-                        <div className="code-snippet__wrapper">
-                            <pre className="code-snippet__body">{user.code}</pre>
-                        </div>
-                    </div>
+              <span className="code-snippet__title">Decoded ID Token</span>
+              <div className="code-snippet__container">
+                <div className="code-snippet__wrapper">
+                  <pre className="code-snippet__body">
+                    {JSON.stringify(user, null, 2)}
+                  </pre>
                 </div>
-          </div> */}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
